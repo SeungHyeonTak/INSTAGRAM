8ece2c3eac16d045808d1249f396863735da1ec4
탁승현 <conficker77@gmail.com>
2021-06-05 18:29:45 +0900

    - model table 구성

---
+++ core/use/admin.py
@@ -0,0 +1,44 @@
+from django.contrib import admin
+from core.use.models import *
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'created_at',)
+    list_display_links = ('id',)
+    search_fields = ('user',)
+    ordering = ('-created_at',)
+
+
+@admin.register(Comments)
+class CommentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'content', 'created_at',)
+    list_display_links = ('id',)
+    search_fields = ('user',)
+    ordering = ('-created_at',)
+
+
+@admin.register(PostLike)
+class PostLikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(CommentsLike)
+class CommentsLikeAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DirectMessage)
+class DirectMessageAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user_from', 'user_to', 'content', 'created_at',)
+    list_display_links = ('id',)
+    search_fields = ('user',)
+    ordering = ('-created_at',)
+
+
+@admin.register(Following)
+class FollowingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'following_user', 'created_at',)
+    list_display_links = ('id',)
+    search_fields = ('user',)
+    ordering = ('-created_at',)
