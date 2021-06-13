4a5adfa75b479ccbeff6f6158e9248d5914d5343
탁승현 <conficker77@gmail.com>
2021-06-13 14:32:54 +0900

    - router url settings edit

--- apps/api/url/account.py
+++ apps/api/url/account.py
@@ -1,10 +1,12 @@
 import apps.api.views as rest_views
 from django.urls import path, include
+from rest_framework import routers
 
 app_name = 'account'
 
-signup = rest_views.SignupViewSet.as_view({'post': 'create'})
+router = routers.DefaultRouter()
+router.register('signup', rest_views.SignupViewSet, basename='signup')  # 회원가입 api
 
 urlpatterns = [
-    path('account/authorization/signup/', signup, name='signup'),  # 회원가입 api
+    path('', include(router.urls)),
 ]
